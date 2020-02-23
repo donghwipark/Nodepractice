bb35de45b23e9370ade36e18c4d4f290b93ca24c
Donghwi Park <donghwipark@gmail.com>
2020-02-23 17:46:12 +0900

    sequelize

--- learn-express/routes/users.js
+++ learn-express/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/flash', function(req, res) {
+  req.session.message = 'session message';
+  req.flash('message', 'flash message');
+  res.redirect('/users/flash/result');
+});
+
+router.get('/flash/result', function(req, res) {
+  req.send(`${req.session.message} ${req.flash('message')}`);
+});
+
 module.exports = router;
--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -3,42 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
-var flash = require('connect-flash');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
+var sequelize = require('./models').sequelize;
 
 var app = express();
+sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(function (req, res, next) {
-  console.log(req.url, 'I am the middleware too');
-  next();
-});
-
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('secret code'));
-
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: 'secret code',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
-app.use(flash());
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
